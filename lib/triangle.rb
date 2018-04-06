f81a06a4dccfec271beb5132f3d9e55b0af67a82
Lucy Wall <lhwall@gmail.com>
2018-04-06 17:59:53 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,6 +9,15 @@ class Triangle
     
   end 
   
+  def sides_positive?
+    @side1 > 0 && @side2 > 0 && @side3 > 0 
+  end 
+  
+  def triangle_valid?
+   @side1 + @side2 > @side3 && @side1 + @side3 > @side2 && @side2 + @side3 > @side1
+  end
+    
+  
   def kind
   end 
   
